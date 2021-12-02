c8e160e4c6c273ffd89c99dd3b0f98afce8069a6
leio10 <leiodd@gmail.com>
2021-12-02 08:53:47 +0000

    nicer code for day 2

--- lib/day2/day2.ex
+++ lib/day2/day2.ex
@@ -22,21 +22,27 @@ defmodule V2021.Day2 do
     |> String.split("\n")
   end
 
-  defp move([], [h | [d]]) do h*d end
+  defp move([], [h, d]) do h*d end
   defp move([step | rest], position) do
-    move(rest, apply_step(String.split(step, " "), position))
+    move(rest, apply_step(parse_step(step), position))
   end
 
-  defp apply_step(["down" | [x]], [h | [d]]) do [h, d+String.to_integer(x)] end
-  defp apply_step(["up" | [x]], [h | [d]]) do [h, d-String.to_integer(x)] end
-  defp apply_step(["forward" | [x]], [h | [d]]) do [h + String.to_integer(x), d] end
+  def parse_step(step) do
+    step
+    |> String.split(" ")
+    |> (fn([direction, x]) -> [direction, String.to_integer(x)] end).()
+  end
 
-  defp move_with_aim([], [h | [d | _]]) do h*d end
+  defp apply_step(["down", x], [h, d]) do [h, d + x] end
+  defp apply_step(["up", x], [h, d]) do [h, d - x] end
+  defp apply_step(["forward", x], [h, d]) do [h + x, d] end
+
+  defp move_with_aim([], [h, d, _]) do h*d end
   defp move_with_aim([step | rest], position) do
-    move_with_aim(rest, apply_step_with_aim(String.split(step, " "), position))
+    move_with_aim(rest, apply_step_with_aim(parse_step(step), position))
   end
 
-  defp apply_step_with_aim(["down" | [x]], [h | [d | [aim]]]) do [h, d, aim+String.to_integer(x)] end
-  defp apply_step_with_aim(["up" | [x]], [h | [d | [aim]]]) do [h, d, aim-String.to_integer(x)] end
-  defp apply_step_with_aim(["forward" | [x]], [h | [d | [aim]]]) do [h + String.to_integer(x), d+aim*String.to_integer(x), aim] end
+  defp apply_step_with_aim(["down", x], [h, d, aim]) do [h, d, aim + x] end
+  defp apply_step_with_aim(["up", x], [h, d, aim]) do [h, d, aim - x] end
+  defp apply_step_with_aim(["forward", x], [h, d, aim]) do [h + x, d + aim * x, aim] end
 end
